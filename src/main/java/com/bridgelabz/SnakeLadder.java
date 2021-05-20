5413544193e31e151f3739b37fe044e10ded8e03
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-05-20 20:14:11 +0530

    ADDED:Player rolls the die

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -4,5 +4,7 @@ public class SnakeLadder {
     public static void main(String args[])
     {
         int position=0;
+        int dieRoll=1+(int)Math.floor(Math.random()*10)%6;
+        System.out.println("Die Rolled:"+dieRoll);
     }
 }

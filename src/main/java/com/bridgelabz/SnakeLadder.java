6be333642e80d2157ec35e7a9e6fa8d2319865d2
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-05-20 20:24:56 +0530

    ADDED:Checks for a option No play, Ladder or Snake

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -6,5 +6,20 @@ public class SnakeLadder {
         int position=0;
         int dieRoll=1+(int)Math.floor(Math.random()*10)%6;
         System.out.println("Die Rolled:"+dieRoll);
+        int optionSelect=1+(int)Math.floor(Math.random()*10)%6;
+        if(optionSelect==1)
+        {
+            System.out.println("--No Play--");
+        }
+        else if(optionSelect==2)
+        {
+            System.out.println("Ladder Comes");
+            position+=dieRoll;
+        }
+        else
+        {
+            System.out.println("Snake Comes");
+            position-=dieRoll;
+        }
     }
 }

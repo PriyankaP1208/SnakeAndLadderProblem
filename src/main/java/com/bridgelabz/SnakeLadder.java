240725a058efb63285433527168058989453b410
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-05-20 20:55:44 +0530

    ADDED:Repeat till the player reaches winning position 100

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -4,22 +4,35 @@ public class SnakeLadder {
     public static void main(String args[])
     {
         int position=0;
-        int dieRoll=1+(int)Math.floor(Math.random()*10)%6;
-        System.out.println("Die Rolled:"+dieRoll);
-        int optionSelect=1+(int)Math.floor(Math.random()*10)%6;
-        if(optionSelect==1)
+        while(position!=100)
         {
-            System.out.println("--No Play--");
-        }
-        else if(optionSelect==2)
-        {
-            System.out.println("Ladder Comes");
-            position+=dieRoll;
-        }
-        else
-        {
-            System.out.println("Snake Comes");
-            position-=dieRoll;
+            int dieRoll=1+(int)Math.floor(Math.random()*10)%6;
+            System.out.println("Die Rolled:"+dieRoll);
+            int optionSelect=1+(int)Math.floor(Math.random()*10)%6;
+            if(optionSelect==1)
+            {
+                System.out.println("No Play");
+            }
+            else if(optionSelect==2)
+            {
+                System.out.println("Ladder Comes");
+                if(position+dieRoll<=100)
+                {
+                    position+=dieRoll;
+                }
+            }
+            else
+            {
+                System.out.println("Snake Comes");
+                if((position-dieRoll)<0)
+                {
+                    position=0;
+                }
+                else
+                {
+                    position-=dieRoll;
+                }
+            }
         }
     }
 }
